cb835c3fd159ad2d649d5c9f3a628d0e1a356c27
Jayeshpawar4912 <jayeshpawar28122003@gmail.com>
2026-01-09 09:15:09 +0530

    Day6new

---
+++ Day6/dictionary.py
@@ -0,0 +1,8 @@
+Dictionary={
+    "Name":"Jayesh",
+    "Age":22,
+    "City":"Nashik",
+    "Skills":["Python","Java","C++"],
+    }
+for key,value in Dictionary.items():
+    print(key," ",value)
---
+++ Day6/list.py
@@ -0,0 +1,12 @@
+list1=[2,5,8,6,4,"Jay","Jayesh"]
+print(list1)
+for i in list1:
+    print(i)
+print(list1[3])
+print(list1[-1])
+print(list1[2:5])
+print(list1[:2])
+print(list1[3:])
+print(len(list1))
+print(list1[:])
+print(list1*2)
